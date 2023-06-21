e49353c9590e2d1f49873c23a722d0daa0e33ec5
Akbar Amin <akbaraminpk@gmail.com>
2023-06-21 11:53:08 +0500

    Initial Commit: Started app

---
+++ app/src/main/java/com/example/note_app/SpActivity.java
@@ -0,0 +1,24 @@
+package com.example.note_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SpActivity extends AppCompatActivity {
+int SPLASH_TIMER =300;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sp);
+
+        Handler handler= new Handler();
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(SpActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, SPLASH_TIMER);
+    }
+}
